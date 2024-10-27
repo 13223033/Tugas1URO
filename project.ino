3df1f5287f7f4f5d34a339897e7be91adc80578f
unknown <16523198@std.stei.itb.ac.id>
2024-10-27 23:27:46 +0700

    fix: ganti ke bahasa indonesia agar lebih mudah dimengerti

--- project.ino
+++ project.ino
@@ -1,9 +1,9 @@
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2); // atur address LCD I2C ke 0x27 untuk tampilan 16 karakter dan 2 baris
 
-// Setup a matrix keypad with 4 rows and 4 columns
+// Mengatur keypad matriks dengan 4 baris dan 4 kolom
 const byte ROWS = 4;
 const byte COLS = 4;
 char keys[ROWS][COLS] = {
@@ -13,15 +13,15 @@ char keys[ROWS][COLS] = {
   {'*','0','#','D'}
 };
 
-// Connect the pins from right to left to pin 2, 3, 4, 5,6,7,8,9
-byte rowPins[ROWS] = {9,8,7,6}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {5,4,3,2}; //connect to the column pinouts of the keypad
+// Hubungkan pin dari kanan ke kiri (di hardware Keypad-nya) ke pin 2, 3, 4, 5, 6, 7, 8, 9
+byte rowPins[ROWS] = {9,8,7,6}; //hubungkan ke pin baris dari keypad
+byte colPins[COLS] = {5,4,3,2}; //hubungkan ke pin kolom dari keypad
 
-// Mapping the key to the keypad
+// Memetakan key ke keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-// Define the initial variables
-String pass = "29012005"; //PASSWORD
+// Mendefinisikan variabel awal
+String pass = "29012005"; // PASSWORD
 int buzz = 11; 
 int alarm = 12;
 int PIR = 13;
@@ -32,70 +32,70 @@ String keyPressed = "";
 void setup() {
   Serial.begin(9600);
 
-  // Initialize the LCD
+  // Inisialisasi LCD
   lcd.init();
   lcd.begin(16, 2);
   lcd.backlight();
 
-  // Define the pinMode
+  // Mendefinisikan pinMode
   pinMode(buzz, OUTPUT);
   pinMode(alarm, OUTPUT);
   pinMode(PIR, INPUT);
 }
 
 void loop() {
-  // Initial screen
+  // Tampilan awal
   if (currentposition == 0) {
     displayscreen();
   }
 
-  // Check if the PIR sensor detects any motion
+  // Periksa apakah sensor PIR mendeteksi gerakan
   if (digitalRead(PIR) == HIGH) {
     digitalWrite(alarm, HIGH);
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("INTRUDER ALERT!");
+    lcd.print("PERINGATAN PENCURI!");
     delay(5000);
     lcd.clear();
     currentposition = 0;
   }
 
-  // Get the key pressed
+  // Get key yang ditekan
   char key = keypad.getKey();
 
-  // Check what key is pressed
-  if (key != NO_KEY && key != '#' && key != '*') { // if a 'number' key is pressed
+  // Periksa key apa yang ditekan
+  if (key != NO_KEY && key != '#' && key != '*') { // jika key berbentuk angka ditekan
     keyPressed.concat(key);
     Serial.print(keyPressed);
 
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("PASSWORD:");
+    lcd.print("KATA SANDI:");
     keypress();
     lcd.setCursor(0, 1);
 
     lcd.print(keyPressed);
     ++currentposition;
-  } else if (key == '#') { // if the '#' key is pressed, for Enter key, and then checking if the password is correct
-    if (keyPressed == pass) { // if the password is correct
+  } else if (key == '#') { // jika key '#' ditekan (sebagai tombol Enter), kemudian memeriksa apakah password benar
+    if (keyPressed == pass) { // jika password benar
       alarmoff();
       currentposition = 0;
       keyPressed = "";
-    } else { // if the password is incorrect
+    } else { // jika password salah
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("INVALID PASSWORD");
+      lcd.print("KATA SANDI SALAH");
       delay(5000);
       currentposition = 0;
       keyPressed = "";
     }
-  } else if (key == '*' and keyPressed.length() > 0) { // if the '*' key is pressed, for Backspace key
+  } else if (key == '*' && keyPressed.length() > 0) { // jika key '*' ditekan, sebagai tombol Backspace
     keyPressed.remove(keyPressed.length() - 1);
     Serial.print(keyPressed);
 
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("PASSWORD:");
+    lcd.print("KATA SANDI:");
     keypress();
     lcd.setCursor(0, 1);
 
@@ -104,27 +104,27 @@ void loop() {
   }
 }
 
-// Function to display the initial screen
+// Fungsi untuk menampilkan layar awal
 void displayscreen() {
   lcd.setCursor(0, 0);
-  lcd.print("ENTER THE CODE");
-  lcd.setCursor(2, 1);
-  lcd.print("TO TURN OFF ALARM!");
+  lcd.print("MASUKKAN KODE");
+  lcd.setCursor(0, 1);
+  lcd.print("UNTUK MEMATIKAN ALARM!");
 }
 
-// Function to make the buzzer sound each time a key get pressed
+// Fungsi untuk membuat buzzer berbunyi setiap kali keypad ditekan
 void keypress() {
   digitalWrite(buzz, HIGH);
   delay(50);
   digitalWrite(buzz, LOW);
 }
 
-// Function to turn off the alarm
+// Fungsi untuk mematikan alarm
 void alarmoff() {
   digitalWrite(alarm, LOW);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("ALARM TURNED OFF");
+  lcd.print("ALARM DIMATIKAN");
   delay(5000);
   lcd.clear();
 }
